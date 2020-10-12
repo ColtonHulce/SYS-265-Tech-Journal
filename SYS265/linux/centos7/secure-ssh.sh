7e36c6e4b687f0e09eadeefccca189e517b76b94
ColtonHulce <colton.hulce@mymail.champlain.edu>
2020-10-11 23:16:29 -0400

    added secure-ssh.sh

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,14 @@
+#secure-ssh.sh
+#author ColtonHulce
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+USER="$1"
+git pull
+sudo useradd -m -d /home/$USER -s /bin/bash $USER
+sudo usermod -aG wheel $USER
+sudo mkdir /home/$USER/.ssh
+sudo cp ~/SYS-265-Tech-Journal/linux/public-keys/id_rsa.pub /home/$USER/.ssh/authorized_keys
+sudo chmod 700 /home/$USER/.ssh
+sudo chmod 600 /home/$USER/.ssh/authorized_keys
+sudo chown -R $USER:$USER /home/$USER/.ssh
